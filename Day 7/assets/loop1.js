d566c16a1a62fdcce25709021fdad7bbe156e1a8
drishtinayyar17985-cmyk <drishtinayyar17985@gmail.com>
2026-06-25 14:32:31 +0530

    Create loop1.js

---
+++ Day 7/assets/loop1.js
@@ -0,0 +1,8 @@
+let ncols=prompt("enter number of columns");
+document.write(`<table width="100%" border="1">
+    <tr>`)
+    for(let n=1;n<=ncols;n++)
+    {
+        document.write(`<td>${n}</td>`)
+    }
+    document.write(`</tr></table>`)
