f68b60f023c13ca2e294b7ce162e22fb967b829d
drishtinayyar17985-cmyk <drishtinayyar17985@gmail.com>
2026-06-26 14:21:03 +0530

    Create obj.js

---
+++ Day8/assets/obj.js
@@ -0,0 +1,17 @@
+let projects=[
+    {pid:1,
+        pname:"Astro App",
+        developer:"David"
+    },
+    
+        {pid:1,
+        pname:"Fintech App",
+        developer:"Raman" 
+    }
+];
+console.log(projects);
+for(let pro of projects)
+{
+    console.log(pro);
+    console.log(pro.developer);
+}
